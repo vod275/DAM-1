577460abc285ecd9e3229991701c0c3d9d200e25
Victor <v.oliver@copermatica.com>
2024-02-19 13:14:30 +0100

    queda 1 commit

--- Deposito/src/cuentas/CCuenta.java
+++ Deposito/src/cuentas/CCuenta.java
@@ -1,15 +1,28 @@
 package cuentas;
 
+/**
+ * Clase para representar una cuenta bancaria.
+ */
 public class CCuenta {
 
-    private String nombre;
-    private String cuenta;
-    private double saldo;
-    private double tipoInterés;
+    private String nombre; // Nombre del titular de la cuenta
+    private String cuenta; // Número de cuenta
+    private double saldo; // Saldo actual de la cuenta
+    private double tipoInterés; // Tipo de interés aplicado a la cuenta
 
+    /**
+     * Constructor por defecto de la clase CCuenta.
+     */
     public CCuenta() {
     }
 
+    /**
+     * Constructor con parámetros de la clase CCuenta.
+     * @param nom Nombre del titular de la cuenta.
+     * @param cue Número de cuenta.
+     * @param sal Saldo inicial de la cuenta.
+     * @param tipo Tipo de interés aplicado a la cuenta.
+     */
     public CCuenta(String nom, String cue, double sal, double tipo) {
         nombre = nom;
         cuenta = cue;
@@ -17,37 +30,35 @@ public class CCuenta {
         tipoInterés = tipo;
     }
 
-    public double getSaldo() {
+    /**
+     * Método para obtener el saldo actual de la cuenta.
+     * @return El saldo actual de la cuenta.
+     */
+    public double estado() {
         return saldo;
     }
 
-    public void setSaldo(double saldo) {
-        this.saldo = saldo;
-    }
-
-    public double getTipoInterés() {
-        return tipoInterés;
-    }
-
-    public void setTipoInterés(double tipoInterés) {
-        this.tipoInterés = tipoInterés;
-    }
-
+    /**
+     * Método para ingresar una cantidad en la cuenta.
+     * @param cantidad La cantidad a ingresar.
+     * @throws Exception Si la cantidad es negativa.
+     */
     public void ingresar(double cantidad) throws Exception {
         if (cantidad < 0)
             throw new Exception("No se puede ingresar una cantidad negativa");
         saldo = saldo + cantidad;
     }
 
+    /**
+     * Método para retirar una cantidad de la cuenta.
+     * @param cantidad La cantidad a retirar.
+     * @throws Exception Si la cantidad es negativa o superior al saldo disponible.
+     */
     public void retirar(double cantidad) throws Exception {
         if (cantidad <= 0)
             throw new Exception("No se puede retirar una cantidad negativa");
         if (estado() < cantidad)
             throw new Exception("No se hay suficiente saldo");
         saldo = saldo - cantidad;
-    }
-
-    public double estado() {
-        return saldo;
     }
 }
